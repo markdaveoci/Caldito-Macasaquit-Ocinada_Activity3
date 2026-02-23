3f2f6566294d24987318ed8af3bcf8450708e4d4
Sean Macasaquit <macasaquitseankevin@gmail.com>
2026-02-23 11:13:30 +0800

    Student Builder Initiation

--- Student.java
+++ Student.java
@@ -71,3 +71,94 @@ public class Student {
         }
 
 }
+
+public static class Builder {
+        private int id = 0;
+        private String firstName;
+        private String middleName;
+        private String lastName;
+        private int age;
+        private String email;
+        private String course;
+        private int yearLevel;
+        private String section;
+        private String school;
+        private String gender;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setCourse(String course) {
+            this.course = course;
+            return this;
+        }
+
+        public Builder setYearLevel(int yearLevel) {
+            this.yearLevel = yearLevel;
+            return this;
+        }
+
+        public Builder setSection(String section) {
+            this.section = section;
+            return this;
+        }
+
+        public Builder setSchool(String school) {
+            this.school = school;
+            return this;
+        }
+
+        public Builder setGender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Student build() {
+            return new Student(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Student " + id +
+                " | " + firstName +
+                " | " + middleName +
+                " | " + lastName +
+                " | " + age +
+                " | " + email +
+                " | " + course +
+                " | " + yearLevel +
+                " | " + section +
+                " | " + school +
+                " | " + gender;
+    }
+
+
+this.id
